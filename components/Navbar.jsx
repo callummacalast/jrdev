56d9e66ae781c17c2b9d3f207ecfd01c4278f966
Callum Macalast <callummacalast@Callums-MacBook-Pro-2.local>
2022-12-03 19:31:51 +0000

    Remove projects from nav

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -104,27 +104,6 @@ const Navbar = () => {
                 </span>
               </a>
             </Link>
-            <Link href="/projects">
-              <a
-                className={`${
-                  router.pathname == '/projects'
-                    ? 'block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-violet-400 md:p-0 dark:text-white'
-                    : 'block py-2 pr-4 pl-3 text-gray-100 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'
-                }`}
-                onClick={handleClick}
-              >
-                Projects
-                <span
-                  className={`${
-                    router.pathname == '/projects'
-                      ? 'text-white'
-                      : 'text-violet-400'
-                  }`}
-                >
-                  .
-                </span>
-              </a>
-            </Link>
             <Link href="../posts">
               <a
                 className={`${
